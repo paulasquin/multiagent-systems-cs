a92cf7d06e86a7a3af265ba6db11eeb810d87636
Paul Asquin <paul.asquin@gmail.com>
2019-01-16 16:53:51 +0100

    adding Max parameters in behaviour, and state display

--- src/SayingHelloBehaviour.java
+++ src/SayingHelloBehaviour.java
@@ -1,18 +1,26 @@
 import jade.core.behaviours.Behaviour;
 
 public class SayingHelloBehaviour extends Behaviour {
+	
 	private int counter = 0;
+	public static final int MAX = 10;
 	
 	@Override
 	public void action()
 	{
+		if (counter == 0)
+			System.out.println("Agent " + ((TestAgent)myAgent).getMyID() + " start");
+		
 		System.out.println("Hello " + counter + " from "  + ((TestAgent)myAgent).getMyID());
 		counter++;
+		
+		if (counter==MAX)
+			System.out.println("Agent " + ((TestAgent)myAgent).getMyID() + " finish");
 	}
 	
 	@Override
 	public boolean done() {
-		return counter >= 10;
+		return counter >= MAX;
 	}
 
 }
